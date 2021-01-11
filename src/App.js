4eacf7cd50c1a00495c4a6aebd7159648febf854
saleembaig <gojalisaleem1@gmail.com>
2021-01-11 16:17:27 +0500

    Parent component
    all child components are called within this and the gloabl transaction provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import { Balance } from './Balance';
+import BalanceSummary from './BalanceSummary';
+import AddTransaction from './AddTransaction';
+import { TransactionProvider } from './TransactionContext';
+import TransactionHistory from './TransactionHistory'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TransactionProvider >
+        <div className="main_div">
+          <div className="center_div">
+            <Header />
+            <div>
+              <Balance />
+              <BalanceSummary />
+              <TransactionHistory />
+              <AddTransaction />
+            </div>
+          </div>
+        </div>
+      </TransactionProvider>
+    </>
+  )
 }
 
 export default App;
